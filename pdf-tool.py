d32a3aa01e20042902f2ed17e5a284960e642423
Arti Karahoda <arti.kh@protonmail.ch>
2018-08-31 01:44:27 +0200

    Added option to provide the password in stdin
    The password can be specified from stdin using the `-p` option instead of `-p <password>` only.

--- pdf-tool.py
+++ pdf-tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import argparse
+import getpass
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 CPURP = '\033[95m'
@@ -10,31 +11,33 @@ CEND = '\033[0m'
 
 parser = argparse.ArgumentParser(usage='%(prog)s [-h] [-e file -p password] [-d file -w wordlist]')
 parser.add_argument('-e','--encrypt',metavar='file', help='Encrypt PDF',default=None)
-parser.add_argument('-p','--password',metavar='password', help='Password to encrypt PDF',default=None)
+parser.add_argument('-p','--password',metavar='password',nargs='?', help='Password to encrypt PDF',default=None)
 parser.add_argument('-d','--decrypt',metavar='file',help='Brute force PDF',default=None)
 parser.add_argument('-w','--wordlist',metavar='wordlist',help='Wordlist to brute force',default=None)
 ns = parser.parse_args()
 
 #Validations
-
-if (len(sys.argv) != 5):
+if (len(sys.argv) != 5 and len(sys.argv) != 4):
         print "Print -h for usage"
         sys.exit()
 
-if len([x for x in (ns.encrypt,ns.password) if x is not None]) == 1:
-	parser.error('encryption and password options must be given together')
-	sys.exit()
+if(str(ns.password) != "None"):
+	if len([x for x in (ns.encrypt,ns.password) if x is not None]) == 1:
+		parser.error('encryption and password options must be given together')
+		sys.exit()
 
 if len([x for x in (ns.decrypt,ns.wordlist) if x is not None]) == 1:
 	parser.error('decryption and wordlist options must be given together')
 	sys.exit()
 
 # Encrypt
-
-if(str(ns.encrypt) != "None" and str(ns.password) != "None"):
+if(str(ns.encrypt) != "None"):
 	try:
 		filename = ns.encrypt
-		password = ns.password
+		if(str(ns.password) != "None"):
+			password = ns.password
+		else:
+			password = getpass.getpass()
 		ofilename = "encrypted_{}".format(filename)
 
 		input_pdf = PdfFileReader(open(ns.encrypt,"rb"))
@@ -47,7 +50,6 @@ if(str(ns.encrypt) != "None" and str(ns.password) != "None"):
 		print "{}[*]{} Error".format(CRED,CEND)
 
 # Brute force
-
 if(str(ns.decrypt) != "None" and str(ns.wordlist) != "None"):
 	pdf = PdfFileReader(open(ns.decrypt,"rb"))
 
